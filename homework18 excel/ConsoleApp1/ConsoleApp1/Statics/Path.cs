4c80f1afff300966b7bdfb030dccb5a24fe7e41d
AdhamPolad <edhempoladzade2@gmail.com>
2024-10-17 18:24:57 +0400

    add some changes

--- homework18 excel/ConsoleApp1/ConsoleApp1/Statics/Path.cs
+++ homework18 excel/ConsoleApp1/ConsoleApp1/Statics/Path.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,8 @@ namespace ConsoleApp1.Statics
 {
     internal static class Path
     {
-        private static string excelPath = @"C:\Users\aser\Desktop\c#\homework\homewrok18 excel\Students.XLSX";
-        private static string logPath = @"C:\Users\aser\Desktop\c#\homework\homewrok18 excel\ConsoleApp1\ConsoleApp1\Logs\";
+        private static string excelPath = @"C:\Users\aser\Desktop\Student Management\homework18 excel\Students.XLSX";
+        private static string logPath = @"C:\Users\aser\Desktop\Student Management\homework18 excel\ConsoleApp1\ConsoleApp1\Logs\";
 
 
         public static string GetExcelPath() { return excelPath; }
--- homework18 excel/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ homework18 excel/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c2d6459cc4b962b834c914e26f16e16098a5db")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
